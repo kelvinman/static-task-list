1bcd57c2f0c92d8886b30d33cd3c9a5e02fd7dc2
Kelvin Man <kelvinman.tk@gmail.com>
2018-02-09 00:30:49 -0800

    add ability to create new tasks with ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,32 @@
 class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create, :update]
+  before_action :set_tasks, only: [:edit, :update]
+  respond_to :html, :js
 
-  def index
-    @tasks = Task.all
+  def new
+    @task = Task.new
   end
+
+  def create
+    @tasks = Task.create(task_params)
+  end
+
+  def update
+    @task.update_attributes(task_params)
+  end
+
+
+  private
+
+    def all_tasks
+      @tasks = Task.all
+    end
+
+    def set_tasks
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:description, :deadline)
+    end
 end
